c80748b9aa1dbf22fc1abc6c7451e3d87d9602d8
bogucek4 <bogucek4@gazeta.pl>
2018-03-08 22:29:37 +0100

    'secondCommitVer'

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import request
+from datetime import datetime
+from uadetector.flask import UADetector
 
 app = Flask(__name__)
-
+UADetector(app)
+ctr = 1
 
 @app.route('/')
 def hello():
@@ -13,6 +16,21 @@ def hello():
 def request_info():
     return f'request method: {request.method} url: {request.url} headers: {request.headers}'
 
+@app.route('/now')
+def now():
+    now = datetime.utcnow()
+    return f'{now}'
+
+@app.route('/user-agent')
+def userAgent():
+    brow = request.user_agent.browser + request.user_agent.version
+    return f' {request.ua.device_type} / {request.user_agent.platform} / {brow}   '
+
+@app.route('/counter')
+def counter():
+    global ctr
+    ctr += 1
+    return f'counter: {ctr}'
 
 if __name__ == '__main__':
     app.run(debug=True)
